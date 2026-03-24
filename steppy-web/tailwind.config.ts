b3c930e3817bcca95b1dd6753d6f2279a4a40c09
roshan06.dev <roshan06.dev@gmail.com>
2026-03-24 21:10:59 +0000

    Initial Next.js setup

---
+++ steppy-web/tailwind.config.ts
@@ -0,0 +1,20 @@
+module.exports = {
+    purge: [],
+    darkMode: false,
+    theme: {
+        extend: {
+            colors: {
+                steppy: {
+                    bg: "#1a1a2e",
+                    teal: "#00c9a7",
+                    blue: "#20a4f3",
+                    panel: "#f1f1f2",
+                }
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
